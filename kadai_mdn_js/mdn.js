d1bd800e52e0182e96c44c8f6170b7f5532c8a39
kuruminchi <166799565+kuruminchi@users.noreply.github.com>
2024-05-11 23:10:19 +0900

    Delete kadai_mdn_js directory

--- kadai_mdn_js/mdn.js
+++
@@ -1,7 +0,0 @@
-const date = new Date();
-const [month, day, year] = [
-  date.getMonth(),
-  date.getDate(),
-  date.getFullYear(),
-];
-console.log(date.getFullYear() + '年', date.getMonth() + '月', date.getDate() + '日');
